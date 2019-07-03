cbb11296bb65b76c942aba25cc169077b9298ed5
Jason Constam <callmebob@gmail.com>
2019-07-03 18:29:56 +0200

    Cleaned up main, moved images for readme

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # C++ Data Structures
 ## Sorting
 ### Insertion Sort
-![Insertion Sort](images/insert.gif)
+![Insertion Sort](images/readme/insert.gif)
 ### Selection Sort
-![Selection Sort](images/select.gif)
+![Selection Sort](images/readme/select.gif)
 ### Bubble Sort
-![Bubble Sort](images/bubble.gif)
+![Bubble Sort](images/readme/bubble.gif)
 ### Merge Sort
-![Merge Sort](images/merge.gif)
+![Merge Sort](images/readme/merge.gif)
 ### Quick Sort
-![Quick Sort](images/quick.gif)
+![Quick Sort](images/readme/quick.gif)
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
-#include <cstring>
+#include <string>
+#include <iostream>
+#include <cstdlib>
 
 #include "gifenc.h"
 #include "data.hpp"
@@ -8,25 +10,48 @@
 #include "sort_merge.hpp"
 #include "sort_quick.hpp"
 
-int main( int argc, char* argv[ ] )
-{
-	dataStor test;
+using namespace std;
 
+static void generateReadmeImages( )
+{
+	const int count = 1000;
+	const int maxValue = 200;
+	const string folder = "images/readme/";
+
+	cout << "Generating ReadMe Images" << endl;
+
+	system( ( "mkdir -p " + folder ).c_str( ) );
+
+	cout << "\tGenerating " << count << " values that are between 0 and " << maxValue << endl;
+	dataStor test;
 	test.generate( 500, 200 );
 
+	cout << "\tInsertion Sort..." << endl;
 	sort_insertion sort_insert( test );
-	sort_insert.doSortWithGif( "images/insert.gif", 5 );
+	sort_insert.doSortWithGif( folder + "insert.gif", 5 );
 	
+	cout << "\tSelection Sort..." << endl;
 	sort_selection sort_select( test );
-	sort_select.doSortWithGif( "images/select.gif", 5 );
+	sort_select.doSortWithGif( folder + "select.gif", 5 );
 
+	cout << "\tMerge Sort..." << endl;
 	sort_merge sort_merge( test );
-	sort_merge.doSortWithGif( "images/merge.gif", 10 );
+	sort_merge.doSortWithGif( folder + "merge.gif", 10 );
 
+	cout << "\tBubble Sort..." << endl;
 	sort_bubble sort_bubble( test );
-	sort_bubble.doSortWithGif( "images/bubble.gif", 1000 );
+	sort_bubble.doSortWithGif( folder + "bubble.gif", 1000 );
 
+	cout << "\tQuick Sort..." << endl;
 	sort_quick sort_quick( test );
-	sort_quick.doSortWithGif( "images/quick.gif", 30 );
+	sort_quick.doSortWithGif( folder + "quick.gif", 30 );
+
+	cout << "\tDone" << endl;
+}
+
+int main( int argc, char* argv[ ] )
+{
+	generateReadmeImages( );
+
 	return 0;
 }
